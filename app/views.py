b29d13c7b552fc7d431a9f75fd66170bde6c9096
Andrey Pachay <apachay@octoberry.ru>
2014-07-26 17:31:01 +0400

    cards demo2

--- app/views.py
+++ app/views.py
@@ -7,19 +7,16 @@ from server import app
 @app.route("/feed", methods=['GET', 'POST'])
 @db_session
 def feed():
-    items = [
-            {
+    articles = [{'id': 1,
                 'type': 'article',
-                'id': 1,
                 'title': u'Тестовый заголовок',
                 'action_title': u'Прочесть',
-                'url': 'http://ya.ru'
-            },
-            {
-                'type': 'advice',
-                'id': 1,
-                'title': u'Тестовый заголовок',
-                'action_title': u'ОК',
-            },
-            ]
+                'url': 'http://ya.ru'}]
+    advices = [{'id': 1,
+               'type': 'advice',
+               'title': u'Тестовый заголовок',
+               'text': u'Тестовый текст',
+               'action_title': u'Согласен'}]
+
+    items = articles + advices
     return jsonify(feed=items)
